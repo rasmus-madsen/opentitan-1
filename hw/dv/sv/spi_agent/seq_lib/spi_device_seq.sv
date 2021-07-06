lowRISC/opentitan

[spi_host, dv] Support smoke test
Signed-off-by: Tung Hoang <hoang.tung@wdc.com>

hw/dv/sv/spi_agent/seq_lib/spi_device_seq.sv
@@ -12,21 +12,44 @@ class spi_device_seq extends spi_base_seq;
 
   spi_item req_q[$];
 
+  // data to be sent
+  rand bit [7:0] data[$];
+
+  // constrain size of data sent / received to be at most 64kB
+  constraint data_size_c {
+    data.size() inside {[1:65536]};
+  }
+
   virtual task body();
-    fork
-      forever begin
-        spi_item  req;
-        p_sequencer.req_analysis_fifo.get(req);
-        req_q.push_back(req);
-      end
-      forever begin
-        spi_item  rsp;
-        wait(req_q.size > 0);
-        rsp = req_q.pop_front();
-        start_item(rsp);
-        finish_item(rsp);
-      end
-    join
+    // if mode = Dual/Quad then adopting re-active slave agent (half duplex)
+    if (cfg.spi_mode != "Single") begin
+      fork
+        forever begin
+          spi_item  req;
+          p_sequencer.req_analysis_fifo.get(req);
+          req_q.push_back(req);
+        end
+        forever begin
+          spi_item  rsp;
+          wait(req_q.size > 0);
+          rsp = req_q.pop_front();
+          start_item(rsp);
+          finish_item(rsp);
+        end
+      join
+    end else begin
+    // if mode = Single then adopt full duplex
+      req = spi_item::type_id::create("req");
+      start_item(req);
+      `DV_CHECK_RANDOMIZE_WITH_FATAL(req,
+                                     item_type == SpiTransNormal;
+                                     data.size() == local::data.size();
+                                     foreach (data[i]) {
+                                       data[i] == local::data[i];
+                                     })
+      finish_item(req);
+      get_response(rsp);
+    end
   endtask : body
   
 endclass : spi_device_seq

hw/dv/sv/spi_agent/spi_device_driver.sv
@@ -9,34 +9,79 @@ class spi_device_driver extends spi_driver;
   virtual task reset_signals();
     forever begin
       @(negedge cfg.vif.rst_n);
+      `uvm_info(`gfn, "\n  spi_device_driver in reset progress", UVM_LOW)
       under_reset = 1'b1;
-      cfg.vif.sio <= 'hz;
+      cfg.vif.sio <= 4'd0;
       @(posedge cfg.vif.rst_n);
       under_reset = 1'b0;
+      `uvm_info(`gfn, "\n  spi_device_driver out of reset", UVM_LOW)
     end
   endtask
 
   virtual task get_and_drive();
     forever begin
+      wait(!under_reset);
       seq_item_port.get_next_item(req);
       $cast(rsp, req.clone());
       rsp.set_id_info(req);
-      `uvm_info(`gfn, $sformatf("spi_host_driver: rcvd item:\n%0s", req.sprint()), UVM_HIGH)
+      `uvm_info(`gfn, $sformatf("  spi_device_driver: req item:\n%0s", req.sprint()), UVM_HIGH)
       fork
-        drive_rx_sio();
-        drive_tx_sio();
+        drive_rx_item(req);
+        drive_tx_item(req);
       join
-      `uvm_info(`gfn, "spi_host_driver: item sent", UVM_HIGH)
+      `uvm_info(`gfn, "  spi_device_driver: item sent", UVM_HIGH)
       seq_item_port.item_done(rsp);
     end
   endtask : get_and_drive
 
-  virtual task drive_rx_sio();
-    // TODO
-  endtask : drive_rx_sio
+  virtual task drive_rx_item(spi_item item);
+    bit [3:0] rx_data;
+    cfg.wait_sck_edge(SamplingEdge);
+    drive_rx_sio(cfg.spi_mode, rx_data);
+  endtask : drive_rx_item
 
-  virtual task drive_tx_sio();
+  virtual task drive_tx_item(spi_item item);
+    bit [7:0] tx_data;
+
     // TODO
+  endtask : drive_tx_item
+
+  virtual task drive_tx_sio(ref spi_mode_e mode, input bit [3:0] tx_data);
+    unique case (mode)
+      Single: begin
+        cfg.vif.sio[1] = tx_data[1];
+      end
+      Dual: begin
+        cfg.vif.sio[3:2] = 2'bzz;
+        cfg.vif.sio[1:0] = tx_data[1:0];
+      end
+      Quad: begin
+        cfg.vif.sio = tx_data;
+      end
+      Reserve: begin
+        cfg.vif.sio = 4'd0;
+      end
+    endcase
   endtask : drive_tx_sio
 
+  virtual task drive_rx_sio(ref spi_mode_e mode, output bit [3:0] rx_data);
+    unique case (mode)
+      Single: begin
+        rx_data[3:2] = 2'bzz;
+        rx_data[1]   = cfg.vif.sio[1];
+        rx_data[0]   = 1'bz;
+      end
+      Dual: begin
+        rx_data[3:2] = 2'bzz;
+        rx_data[1:0] = cfg.vif.sio[1:0];
+      end
+      Quad: begin
+        rx_data = cfg.vif.sio;
+      end
+      Reserve: begin
+        rx_data = 4'bzzzz;
+      end
+    endcase
+  endtask : drive_rx_sio
+  
 endclass : spi_device_driver

hw/ip/spi_host/dv/env/spi_host_env_cfg.sv
@@ -30,6 +30,8 @@ class spi_host_env_cfg extends cip_base_env_cfg #(.RAL_T(spi_host_reg_block));
     m_spi_agent_cfg.ok_to_end_delay_ns = 8000;
     // create req_analysis_fifo for re-active slave agent
     m_spi_agent_cfg.has_req_fifo = 1'b1;
+    // default spi_mode
+    m_spi_agent_cfg.spi_mode = Single;
 
     // create the seq_cfg
     seq_cfg = spi_host_seq_cfg::type_id::create("seq_cfg");
@@ -59,7 +61,6 @@ class spi_host_env_cfg extends cip_base_env_cfg #(.RAL_T(spi_host_reg_block));
     end else begin  // bus and core has same clock frequency
       clk_core_mhz = clk_rst_vif.clk_freq_mhz;
     end
-
     return clk_core_mhz;
   endfunction : get_clk_core_freq
   

rasmus-madsen/opentitan-1

hw/dv/sv/spi_agent/seq_lib/spi_device_seq.sv
@@ -12,21 +12,44 @@ class spi_device_seq extends spi_base_seq;
 
   spi_item req_q[$];
 
+  // data to be sent
+  rand bit [7:0] data[$];
+
+  // constrain size of data sent / received to be at most 64kB
+  constraint data_size_c {
+    data.size() inside {[1:65536]};
+  }
+
   virtual task body();
-    fork
-      forever begin
-        spi_item  req;
-        p_sequencer.req_analysis_fifo.get(req);
-        req_q.push_back(req);
-      end
-      forever begin
-        spi_item  rsp;
-        wait(req_q.size > 0);
-        rsp = req_q.pop_front();
-        start_item(rsp);
-        finish_item(rsp);
-      end
-    join
+    // if mode = Dual/Quad then adopting re-active slave agent (half duplex)
+    if (cfg.spi_mode != "Standard") begin
+      fork
+        forever begin
+          spi_item  req;
+          p_sequencer.req_analysis_fifo.get(req);
+          req_q.push_back(req);
+        end
+        forever begin
+          spi_item  rsp;
+          wait(req_q.size > 0);
+          rsp = req_q.pop_front();
+          start_item(rsp);
+          finish_item(rsp);
+        end
+      join
+    end else begin
+    // if mode = Single then adopt full duplex
+      req = spi_item::type_id::create("req");
+      start_item(req);
+      `DV_CHECK_RANDOMIZE_WITH_FATAL(req,
+                                     item_type == SpiTransNormal;
+                                     data.size() == local::data.size();
+                                     foreach (data[i]) {
+                                       data[i] == local::data[i];
+                                     })
+      finish_item(req);
+      get_response(rsp);
+    end
   endtask : body
   
 endclass : spi_device_seq

hw/dv/sv/spi_agent/spi_device_driver.sv
@@ -9,34 +9,79 @@ class spi_device_driver extends spi_driver;
   virtual task reset_signals();
     forever begin
       @(negedge cfg.vif.rst_n);
+      `uvm_info(`gfn, "\n  spi_device_driver in reset progress", UVM_LOW)
       under_reset = 1'b1;
-      cfg.vif.sio <= 'hz;
+      cfg.vif.sio <= 4'd0;
       @(posedge cfg.vif.rst_n);
       under_reset = 1'b0;
+      `uvm_info(`gfn, "\n  spi_device_driver out of reset", UVM_LOW)
     end
   endtask
 
   virtual task get_and_drive();
     forever begin
+      wait(!under_reset);
       seq_item_port.get_next_item(req);
       $cast(rsp, req.clone());
       rsp.set_id_info(req);
-      `uvm_info(`gfn, $sformatf("spi_host_driver: rcvd item:\n%0s", req.sprint()), UVM_HIGH)
+      `uvm_info(`gfn, $sformatf("  spi_device_driver: req item:\n%0s", req.sprint()), UVM_HIGH)
       fork
-        drive_rx_sio();
-        drive_tx_sio();
+        drive_rx_item(req);
+        drive_tx_item(req);
       join
-      `uvm_info(`gfn, "spi_host_driver: item sent", UVM_HIGH)
+      `uvm_info(`gfn, "  spi_device_driver: item sent", UVM_HIGH)
       seq_item_port.item_done(rsp);
     end
   endtask : get_and_drive
 
-  virtual task drive_rx_sio();
-    // TODO
-  endtask : drive_rx_sio
+  virtual task drive_rx_item(spi_item item);
+    bit [3:0] rx_data;
+    cfg.wait_sck_edge(SamplingEdge);
+    drive_rx_sio(cfg.spi_mode, rx_data);
+  endtask : drive_rx_item
 
-  virtual task drive_tx_sio();
+  virtual task drive_tx_item(spi_item item);
+    bit [7:0] tx_data;
+
     // TODO
+  endtask : drive_tx_item
+
+  virtual task drive_tx_sio(ref spi_mode_e mode, input bit [3:0] tx_data);
+    unique case (mode)
+      Standard: begin
+        cfg.vif.sio[1] = tx_data[1];
+      end
+      Dual: begin
+        cfg.vif.sio[3:2] = 2'bzz;
+        cfg.vif.sio[1:0] = tx_data[1:0];
+      end
+      Quad: begin
+        cfg.vif.sio = tx_data;
+      end
+      RsvdSpd: begin
+        cfg.vif.sio = 4'd0;
+      end
+    endcase
   endtask : drive_tx_sio
 
+  virtual task drive_rx_sio(ref spi_mode_e mode, output bit [3:0] rx_data);
+    unique case (mode)
+      Standard: begin
+        rx_data[3:2] = 2'bzz;
+        rx_data[1]   = cfg.vif.sio[1];
+        rx_data[0]   = 1'bz;
+      end
+      Dual: begin
+        rx_data[3:2] = 2'bzz;
+        rx_data[1:0] = cfg.vif.sio[1:0];
+      end
+      Quad: begin
+        rx_data = cfg.vif.sio;
+      end
+      RsvdSpd: begin
+        rx_data = 4'bzzzz;
+      end
+    endcase
+  endtask : drive_rx_sio
+  
 endclass : spi_device_driver

hw/ip/spi_host/dv/env/spi_host_env_cfg.sv
@@ -31,6 +31,8 @@ class spi_host_env_cfg extends cip_base_env_cfg #(.RAL_T(spi_host_reg_block));
     m_spi_agent_cfg.ok_to_end_delay_ns = 8000;
     // create req_analysis_fifo for re-active slave agent
     m_spi_agent_cfg.has_req_fifo = 1'b1;
+    // default spi_mode
+    m_spi_agent_cfg.spi_mode = Standard;
 
     // create the seq_cfg
     seq_cfg = spi_host_seq_cfg::type_id::create("seq_cfg");
@@ -60,7 +62,6 @@ class spi_host_env_cfg extends cip_base_env_cfg #(.RAL_T(spi_host_reg_block));
     end else begin  // bus and core has same clock frequency
       clk_core_mhz = clk_rst_vif.clk_freq_mhz;
     end
-
     return clk_core_mhz;
   endfunction : get_clk_core_freq
   